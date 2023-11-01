58777607e3d0023c1efae7b0ddd7be84ad72e626
Leo Barnett <leo.e.barnett@gmail.com>
2023-11-01 12:35:53 +0200

    Initial Commit

---
+++ BurpeeKing/BurpeeKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurpeeKingApp.swift
+//  BurpeeKing
+//
+//  Created by Leo  on 01.11.23.
+//
+
+import SwiftUI
+
+@main
+struct BurpeeKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
