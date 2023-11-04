6ca53a54579ee020a1561d7a4ec24d16472a1c74
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-04 22:14:05 +0200

    Version 1.0 - 1RM and % of that

--- BurpeeKing/ContentView.swift
+++ BurpeeKing/ContentView.swift
@@ -1,24 +1,59 @@
-//
-//  ContentView.swift
-//  BurpeeKing
-//
-//  Created by Leo  on 01.11.23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+   
+    @State private var exercise = ""
+    @State private var weightPercentage = 50 // Standardwert auf 50%
+    @State private var weight = 0.0
+    @FocusState private var weightIsFocused: Bool
+    
+    var exerciseWeight: Double {
+        let weightSelection = Double(weightPercentage)
+        let exerciseValue = weight * weightSelection / 100
+        
+        return exerciseValue
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section("Insert exercise") {
+                    TextField("Exercise", text: $exercise)
+                }
+                
+                Section("Add your 1RM") {
+                    TextField("1RM", value: $weight, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($weightIsFocused)
+                }
+                
+                Section("% of 1RM") {
+                    Picker("1RM Percentage", selection: $weightPercentage) {
+                        ForEach(0..<101) {
+                            Text("\($0)% of 1RM")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text("\(exerciseWeight, specifier: "%.2f")")
+                }
+            }
+            .navigationTitle("BurpeeKing")
+            .toolbar {
+                if weightIsFocused {
+                    Button("Done") {
+                        weightIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
